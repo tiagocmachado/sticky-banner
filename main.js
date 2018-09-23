3748bb6f9e5ef3525bdc370380442a340a0d53b5
Tiago Machado <tiago.machado3@pt.bosch.com>
2018-09-23 20:25:36 +0200

    Converted to IE freindly

--- main.js
+++ main.js
@@ -5,56 +5,64 @@ const target = '#the-target';
 const accordion = '#accordion';
 const targetMargin = 30;
 
-const init = () => {
-    const getElementHeight = element => document.querySelector(element).getBoundingClientRect().height;
-    const getElementBottom = element => document.querySelector(element).getBoundingClientRect().bottom;
-    const getElementTop = element => document.querySelector(element).getBoundingClientRect().top;
+function init() {
+    const getElementHeight = function (element) {
+        return document.querySelector(element).getBoundingClientRect().height
+    };
+    const getElementBottom = function (element) {
+        return document.querySelector(element).getBoundingClientRect().bottom
+    };
+    const getElementTop = function (element) {
+        return document.querySelector(element).getBoundingClientRect().top
+    };
     const rightColSelector = document.querySelector(rightCol);
     const mutationObserverConfig = {attributes: true, childList: true, subtree: true};
     const navbarHeight = getElementHeight(navbar);
     const sidebarHeight = getElementHeight(target);
     const sidebarSelector = document.querySelector(target);
     let stuckOffsetBottom = getElementBottom(leftCol) - targetMargin;
-    let sidebarOffsetTop = getElementBottom(accordion) + window.scrollY;
+    let sidebarOffsetTop = getElementBottom(accordion) + document.documentElement.scrollTop;
     let leftColumnHeight = getElementHeight(leftCol);
     let rightColumnHeight = getElementHeight(rightCol);
 
-    const fixBanner = () => {
+    function fixBanner() {
+
         if (leftColumnHeight > rightColumnHeight) {
-            if (window.scrollY + navbarHeight > sidebarOffsetTop) {
-                sidebarSelector.classList.add("fixed");
+            if (document.documentElement.scrollTop + navbarHeight > sidebarOffsetTop) {
+                sidebarSelector.classList.add('fixed');
             } else {
-                sidebarSelector.classList.remove("fixed");
+                sidebarSelector.classList.remove('fixed');
             }
 
             if (sidebarHeight + navbarHeight >= stuckOffsetBottom) {
-                sidebarSelector.style.top = sidebarHeight - stuckOffsetBottom < 0 ? Math.abs(sidebarHeight - stuckOffsetBottom) + "px" : "-" + (sidebarHeight - stuckOffsetBottom) + "px";
+                sidebarSelector.style.top = sidebarHeight - stuckOffsetBottom < 0 ? Math.abs(sidebarHeight - stuckOffsetBottom) + 'px' : '-' + (sidebarHeight - stuckOffsetBottom) + 'px';
             } else {
-                sidebarSelector.style.top = navbarHeight + "px";
+                sidebarSelector.style.top = navbarHeight + 'px';
             }
         }
-    };
+    }
 
-    const observerCallback = () => {
+    function observerCallback() {
         leftColumnHeight = getElementHeight(leftCol) + getElementTop(leftCol);
         rightColumnHeight = getElementHeight(rightCol) + getElementTop(rightCol);
-    };
+    }
+
     const columnObserver = new MutationObserver(observerCallback);
 
     columnObserver.observe(rightColSelector, mutationObserverConfig);
 
     fixBanner();
 
-    window.onscroll = () => {
+    window.onscroll = function () {
         stuckOffsetBottom = getElementBottom(leftCol) - targetMargin;
-        sidebarOffsetTop = getElementBottom(accordion) + window.scrollY;
+        sidebarOffsetTop = getElementBottom(accordion) + document.documentElement.scrollTop;
 
         fixBanner();
     };
-};
+}
 
 document.onreadystatechange = () => {
-    if (document.readyState === "interactive") {
+    if (document.readyState === 'complete') {
         init();
     }
 };
